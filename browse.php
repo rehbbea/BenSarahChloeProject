b90a1295dd97d0e9fa589aa784d87de1399fd7fd
saztronaut <rmhisrk@ucl.ac.uk>
2016-03-05 15:42:35 +0000

    browse and stylesheet updated
    search function in but returns full list if doesn't find anything

--- browse.php
+++ browse.php
@@ -1,4 +1,6 @@
-<?php include 'database.php'; ?>
+<?php include 'database.php'; 
+?>
+
 <?php
    session_start();
 ?>
@@ -6,7 +8,7 @@
 <html>
   <head>
     <meta charset="utf-8"/>
-    <title>Login</title>
+    <title>What's for sale?</title>
 	<link href="../../stylesheet.css" rel="stylesheet" type="text/css">
   </head>
   <body>
@@ -19,30 +21,96 @@
 	<h4>My Auctions</h4></div>
 	<div id=section>
 	<p align="right"><?php include 'loggedon.php'; ?></p>
-	<h3>Browse items</h3> 
-      <div>
-	  
+	<h3> Browse items</h3> 
          <?php
-            $msg = '';
+            $msg = ''; 
+		$defaultq="SELECT u.d_name, a.date_listed, a.date_expires, a.item_name, c.cat_desc, a.description, a.highest_bid 
+			FROM (t_auctions as a JOIN t_sellers as s) JOIN t_users as u, t_cat as c
+			WHERE a.seller_id =s.user_id AND s.user_id=u.user_id AND a.cat= c.cat_id AND
+		date_expires>NOW() ORDER BY(date_expires);";
+		$currentq=$defaultq
+		?>	
+      <div>
+		<div class = "container"> 
+         <form class = "form-search" role = "form" 
+            action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']); 
+            ?>" method = "post">
+            <h4 class = "form-search-heading"></h4>
+			<p><label>Search by keyword:</label><input type = "text" class = "form-control" 
+               name = "search" placeholder = "What are you looking for?" 
+             ><button class = "btn btn-lg btn-primary btn-block" type = "submit" 
+               name = "searchbtn">Go!</button></p>
 
+         </form>
+		 </div>	  
 
-      function findList()
+         <?php
+			if (isset($_POST['searchbtn'])  && !empty($_POST['search'])) {
+			$search = $_POST['search'];
+	        $lookup = "SELECT u.d_name, a.date_listed, a.date_expires, a.item_name, c.cat_desc, a.description, a.highest_bid 
+			FROM (t_auctions as a JOIN t_sellers as s) JOIN t_users as u, t_cat as c
+			WHERE a.seller_id =s.user_id AND s.user_id=u.user_id AND a.cat= c.cat_id AND
+		date_expires>NOW() AND (lower(description) like lower('%" . $search . "%') OR lower(item_name) like lower('%" . $search . "%')) ORDER BY(date_expires);";
+	        if(!mysqli_query($connection, $lookup) | mysqli_query($connection, $lookup)->num_rows < 1) {
+				echo "Sorry, there is nothing under that description";
+			}
+			else
+			{
+				$currentq=$lookup;
+				
+			}
+
+            }
+            
+      function drawTableContents($query)
 		{
-		$connection = mysqli_connect("localhost", "root", "PAmG7Up6wy2ZcEAu", "auctions");
-		 /*introduce password hashing here*/
- 		$query = "SELECT * FROM t_auctions WHERE date_expires>NOW() ORDER BY(date_expires);";
+		include 'database.php'; 
         $result = mysqli_query($connection, $query)
-          or die('Error making saveToDatabase query' . mysql_error());
-		 
-        mysqli_close($connection);
-		return $result;
-      }	
-    if (!is_null(findList()))
-      {
-        $results = findList();
-        echo "<h4> Results from the query need to go here</h4>";
+          or die("Hmm, that didn't seem to work" . mysql_error());
 		
-      }
+		$first = true;
+		
+		$colnames = array(
+		"d_name" =>"Display name", 
+		"date_listed" => "Date listed",
+		"date_expires" => "End of auction",
+		"item_name" => "Name",
+		"cat_desc" => "Category",
+		"description" => "Description",
+		"highest_bid" => "Highest bid");		
+		
+		while($row = mysqli_fetch_array($result,MYSQLI_ASSOC))
+		{
+			
+			if($first){
+				echo "<tr>";
+				foreach($colnames as $column => $value){
+					
+				echo "<th>$value</th>";
+				}
+				echo "</tr>";
+				$first = false;
+			}
+		
+			echo "<tr>";
+			foreach($colnames as $column => $value){
+				echo "<td>";
+				
+				echo $row[$column];
+				echo "</td>";
+				}
+			echo "</tr>";
+		}
+		
+		// Associative array
+		// Free result set
+		mysqli_free_result($result);		 
+        mysqli_close($connection);
+      }	
+   
+		echo "<table>";
+			drawTableContents($currentq);
+		echo "</table>";
          ?>
       </div> <!-- /container -->
 
