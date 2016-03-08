f4783b59f6b0db6ae72ba3a21675825e2f733303
saztronaut <rmhisrk@ucl.ac.uk>
2016-03-08 21:33:09 +0000

    Home shows total owed

--- home.php
+++ home.php
@@ -35,8 +35,8 @@
 		 
 		 
             $msg = ''; 
-		$defaultq="SELECT u.d_name, a.date_listed, a.date_expires, a.item_name, a.auction_id, c.cat_desc, a.description, h.currentval, a.reserve_price, a.sent, a.s_feedback, a.s_paid
-		FROM (SELECT auctionid, max(`amount`) as currentval FROM `t_bids` GROUP BY auctionid) as h RIGHT JOIN t_auctions as a ON 			a.auction_id=h.auctionid, t_sellers as s, t_users as u, t_cat as c 
+		$defaultq="SELECT h.d_name, a.date_listed, a.date_expires, a.item_name, a.auction_id, c.cat_desc, a.description, h.currentval, a.reserve_price, a.sent, a.s_feedback, a.s_paid
+		FROM (SELECT auctionid, max(`amount`) as currentval, d_name FROM `t_bids` as b, t_users as us WHERE b.buyer_id=us.user_id GROUP BY auctionid) as h RIGHT JOIN t_auctions as a ON 			a.auction_id=h.auctionid, t_sellers as s, t_users as u, t_cat as c 
 			where a.seller_id =s.user_id AND s.user_id=u.user_id AND a.cat= c.cat_id AND
 		date_expires<NOW() AND a.auction_id IN (SELECT auction_id FROM t_auctions WHERE seller_id = " . $userid . ") ORDER BY(date_expires);";
 		$currentq=$defaultq;
@@ -48,21 +48,22 @@
 		or die("Hmm, that didn't seem to work" . mysql_error());
 		
 		$first = true;
+		$total_owe=0;
 		
 		$colnames = array(
-		"d_name" =>"Display name", 
-		"date_listed" => "Date listed",
+		/*"date_listed" => "Date listed", Do we need this?*/
 		"date_expires" => "End of auction",
 		"item_name" => "Name",
-		"cat_desc" => "Category",
+		/*"cat_desc" => "Category",   Do we need this?*/
 		"description" => "Description",
 		"currentval" => "Highest bid",
+		"d_name" =>"Winner name", 		
 		"reserve_price" => "Success",
 		"sent"=>  "Sent item?",
 		"s_feedback"=>  "Feedback for seller",	
 		"s_paid"=>  "Payment for listing?",		
-		);		
-		
+		);				
+		echo "<table>";
 		while($row = mysqli_fetch_array($result,MYSQLI_ASSOC))
 		{
 			
@@ -90,7 +91,16 @@
 					 echo "Success";
 					 }
 				}
-
+				else if($column == 'd_name')
+				{
+					if (!($row['reserve_price']>$row['currentval'])){
+						echo $row['d_name'];
+						}
+					else {
+						echo "No one won";
+					 }
+				}
+				
 				else if($column == 'sent')
 				{
 					if (!($row['reserve_price']>$row['currentval'])){
@@ -119,11 +129,13 @@
 					if (!($row['reserve_price']>$row['currentval'])){
 						/* if auction succesful charge 2% of the total cost and 20p for the listing*/
 						$row['owe']= $row['currentval']*0.02+0.2;
+						
 						}
 					else  {
 						$row['owe']=0.2;
 						}
 					echo "£ " . number_format($row['owe'],2);
+					$total_owe= $total_owe+ $row['owe'];
 					 }
 				}
 				else
@@ -134,17 +146,26 @@
 			}
 			}
 			echo "</tr>";
+			
+			
+			
 		}
 		
+		echo "</table>";
+		if ($total_owe>0){
+			 echo "You owe £ " . number_format($total_owe,2) . " altogether";
+			 
+			 
+			}
 		// Associative array
 		// Free result set
 		mysqli_free_result($result);		 
         mysqli_close($connection);
       }	
    
-		echo "<table>";
 			drawTableContents($currentq);
-		echo "</table>";
+		
+		
          ?>
       </div> <!-- /container -->
 
